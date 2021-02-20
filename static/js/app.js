06cd52e2ec7d1bcfea8943483c3276c7c9f45c63
Dainty Cuevas <73034182+daintycuevas@users.noreply.github.com>
2021-02-19 21:28:46 -0500

    update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,24 +1,3 @@
-// const xhr = new XMLHttpRequest();
-// const url = "http://robdunnlab.com/projects/belly-button-biodiversity/";
-
-// xhr.open('GET', url);
-// xhr.onreadystatechange = someHandler;
-// xhr.send();
-
-
-// var thead = d3.select("#sample-metadata");
-// var tbody = d3.select("#sample-metadata");
-
-// function someHandler() {
-//     d3.json(url).then(metadata => {
-//         // var head_row = thead.append("tr");
-//         // head_row.append("th").text(value);
-//         var row = tbody.append("tr");
-//         metadata.forEach(value => {
-//             row.append("td").text(value);
-//         })
-//     })
-// }
 
 d3.json("static/js/samples.json").then(data => {
   console.log(data)
