a394507be402253758f4fe26813a04d305d86d68
Dainty Cuevas <73034182+daintycuevas@users.noreply.github.com>
2021-02-19 21:18:48 -0500

    update folders

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,25 @@
-url = "http://robdunnlab.com/projects/belly-button-biodiversity/"
+// const xhr = new XMLHttpRequest();
+// const url = "http://robdunnlab.com/projects/belly-button-biodiversity/";
 
-var thead = d3.select("#sample-metadata");
-var tbody = d3.select("#sample-metadata");
+// xhr.open('GET', url);
+// xhr.onreadystatechange = someHandler;
+// xhr.send();
 
-d3.json(url).then(metadata => {
-    var head_row = thead.append("tr")
-    head_row.append("th").text(value)
-    var row = tbody.append("tr");
-    metadata.forEach(value => {
-        row.append("td").text(value)
-    })
-})
+
+// var thead = d3.select("#sample-metadata");
+// var tbody = d3.select("#sample-metadata");
+
+// function someHandler() {
+//     d3.json(url).then(metadata => {
+//         // var head_row = thead.append("tr");
+//         // head_row.append("th").text(value);
+//         var row = tbody.append("tr");
+//         metadata.forEach(value => {
+//             row.append("td").text(value);
+//         })
+//     })
+// }
+
+d3.json("static/js/samples.json").then(data => {
+  console.log(data)
+});
