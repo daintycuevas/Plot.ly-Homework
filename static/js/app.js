48c6212c51aa5ece9da08392ca2e9b4c68f0c076
Dainty Cuevas <73034182+daintycuevas@users.noreply.github.com>
2021-02-21 00:24:02 -0500

    update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,44 @@
 // Select tbody for samples.json
 var tbody = d3.select("#sample-metadata");
 
-
 d3.json("static/js/samples.json").then(sampleData => {
   console.log(sampleData)
 
-  Object.entries(sampleData).forEach(names => {
-    console.log(names);
-  })
+  var names = Object.values(sampleData)[0];
+  console.log(names);
 
-  // var subjectID = d3.nest()
-  //   .values(function(d) {
-  //     return d.names;
-  //   })
-   
-  
-  // var idNames = JSON.stringify(subjectID)
-  // console.log(idNames);
-  
+  var metadata = Object.values(sampleData)[1];
+  console.log(metadata);
+
+  var samples = Object.values(sampleData)[2];
+  console.log(samples);
+
+    
+    // var idNames = data.forEach(names => console.log(`${names.data}`));
+
+    // var metaData = data.map(metadata => metadata.data);
+    // sampleMetadata.push(metaData);
+
+    // var sampleData = data.map(samples => samples.data);
+    // dataSamples.push(sampleData);
   
 })
+
+
+    // item += data;
+    // sampleNames.push(names.data[1]);
+    // sampleMetadata.push(metadata.data);
+    // dataSamples.push(samples.data);
+    
+
+  // var dataDropdown = d3.select("#selDataset")
+  //   .append("option")
+  //   .data()
+  // })
+
+  // var names = 
+
+  // var dataDropdown = d3.select("#selDataset")
+  //   .append("option")
+  //   .data()
+
