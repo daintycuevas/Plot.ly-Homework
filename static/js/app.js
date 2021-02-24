68c254a98839cbf9f48699745fff2e1d4b2bd6e8
Dainty Cuevas <73034182+daintycuevas@users.noreply.github.com>
2021-02-23 22:07:27 -0500

    update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -16,22 +16,36 @@ d3.json("static/js/samples.json").then(sampleData => {
   console.log(samples);
   });
 
+
 function plotData(id) {
+
   d3.json("static/js/samples.json").then(sampleData => {
     console.log(sampleData)
 
-    var wfreq = sampleData.metadata.map(d => d.wfreq)
+  var wfreq = sampleData.metadata.map(d => d.wfreq)
     console.log(wfreq)
 
-    var samples = sampleData.samples.filter(s => s.id.toString() === id)[0];
+  var samples = sampleData.samples.filter(s => s.id.toString() === id)[0];
     console.log(samples)
 
-    //filtered id values for the bar chart
-    var sampleValues = samples.sample_values.slice(0, 10)
+  //filtered id values for the bar chart
+  var sampleValues = samples.sample_values.slice(0, 10)
 
-    //labels for the bar chart
-    var otuID = samples.otu_ids.slice(0, 10);
+  //labels for the bar chart
+  var otuID = samples.otu_ids.slice(0, 10);
 
-    //hovertext for the chart
-    var otuLabel = sampleData.otu_labels.slice(0, 10);
+  //hovertext for the chart
+  var otuLabel = sampleData.otu_labels.slice(0, 10);
 
+
+  //========= 
+  //  PLOT
+  //=========
+
+
+  //create trace 
+  
+
+  //create data 
+
+  //create layout
